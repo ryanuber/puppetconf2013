41a6643700b73938d03ce0db94e47db9cddc7c05
Ryan Uber <ru@ryanuber.com>
2013-08-20 16:35:02 -0700

    Updates

--- deck.sh
+++ deck.sh
@@ -2,17 +2,22 @@
 source ./slide.sh
 shopt -s expand_aliases
 
-# configuration
-software_dir=$HOME/puppetconf/packages
-
 function banner() {
     echo -e "\n!!center\n$@\n!!nocenter\n!!sep\n"
 }
 
-function pause(){
+function pause() {
     read -p "Press enter to continue"
 }
 
+function run() {
+    echo "---"
+    echo "\$ ${@}"
+    ${@}
+    echo "---"
+}
+
+###############################################################################
 slide <<EOF
 
 
@@ -23,6 +28,7 @@ slide <<EOF
 Speaker: Ryan Uber
 EOF
 
+###############################################################################
 slide <<EOF
 $(banner "3 Years of Puppet at Cisco")
 
@@ -42,7 +48,7 @@ Covered Topics:
     * Miscellaneous practices and lessons learned
 EOF
 
-############### installing puppet code
+###############################################################################
 slide <<EOF
 $(banner "How we install puppet code on machines")
 
@@ -57,15 +63,16 @@ By using RPM's, our puppet code gets the following for free:
     - Ability to pull in other packages as dependencies
 EOF
 
+###############################################################################
 slide <<EOF
 $(banner "Using RPM packages for distributing puppet code")
 
 For this example, I have the following modules:
 
-$(ls $software_dir)
+$(run "ls cfgmod-*.rpm")
 
 !!pause
-We use the "cfgmod-" prefix to distinguish pacakges that contain runnable
+We use a generic prefix to distinguish pacakges that contain runnable
 puppet module code.
 
 !!pause
@@ -73,6 +80,7 @@ Each module does a small job, but does it very well.
 This way, our modules are highly reusable between projects.
 EOF
 
+###############################################################################
 slide <<EOF
 $(banner "Module Dependencies")
 
@@ -80,17 +88,17 @@ $(banner "Module Dependencies")
 Often times, a module requires some other piece of software.
 
 The "cfgmod-cron" module contains puppet code that configures:
-    - cron
-    - monit
+    - The cron daemon
+    - A monit job to watch the daemon process
 
 !!pause
 cron and monit are required if this module is going to be installed:
 
-rpm -qp --requires cfgmod-cron-*.rpm
-$(rpm -qp --requires $software_dir/cfgmod-cron*.rpm)
+$(run "rpm -qp --requires cfgmod-cron*.rpm")
 
 EOF
 
+###############################################################################
 slide <<EOF
 $(banner "Choosing a place to install modules")
 
