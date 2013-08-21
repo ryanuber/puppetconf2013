7c065facb878fe6b2269bb7e9353e6469ab394b1
Ryan Uber <ru@ryanuber.com>
2013-08-21 01:16:28 -0700

    updates

--- prep.sh
+++ prep.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 [ $(id -u) -eq 0 ] || exit 1
 cd $HOME
-rm -rf repo > /dev/null
-rm -f /etc/yum.repos.d/*.repo > /dev/null
-cp *.repo /etc/yum.repos.d > /dev/null
-tar -zxvf repo.tar.gz > /dev/null
-rpm -e cfgmod-cron > /dev/null
-rpm -e cowsay > /dev/null
-yum -d 0 -y install unzip > /dev/null
-chkconfig crond off > /dev/null
-puppet module uninstall ryanuber-packagelist > /dev/null
-rm -f /root/packages.list > /dev/null
-rm -f /etc/monit.d/cron > /dev/null
+
+# clean up
+rm -rf \
+    repo \
+    /etc/yum.repos.d/* \
+    /root/packages.list \
+    /etc/monit.d/cron
+puppet module uninstall ryanuber-packagelist > /dev/null 2>&1
+rpm -e cowsay cfgmod-cron > /dev/null 2>&1
+
+# set up
 for pkg in cronie monit tree; do if ! rpm -q $pkg &>/dev/null; then exit 1; fi; done
-
-
+cp *.repo /etc/yum.repos.d
+tar -zxf repo.tar.gz
+yum -d 0 -y install unzip > /dev/null
+chkconfig crond off
